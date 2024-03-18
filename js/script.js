33efb18e9e357888c1dfa4252406b084a65f6f3f
Dell <andrewavlasovich@gmail.com>
2024-03-18 14:26:11 +0200

    hw-6 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 'use strict'
+
+// Task 1
+let row1 = prompt('First row');
+let row2 = prompt('Second row');
+let row3 = prompt('Third row');
+console.log(row1+row2+row3);
+
+// Task 2
+
+const a = '12345';
+const str = a.split('');
+console.log(str);
